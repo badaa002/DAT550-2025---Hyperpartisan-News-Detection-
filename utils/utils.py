70efa3d683d952f148d8eaa701cb2d31f062428a
Mikah Florian Damen <92051984+olivrverhelst@users.noreply.github.com>
2025-04-02 11:10:17 +0200

    Add files via upload
    Added a print statement for auc

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc
 import matplotlib.pyplot as plt
 
-
 def load_cleaned_data(file_to_load):
     return pd.read_csv(file_to_load, sep='\t')
 
@@ -12,8 +11,8 @@ def split(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     return X_train, X_test, y_train, y_test
 
-
 def metrics(y_test, y_pred, y_pred_proba):
+    
     accuracy = accuracy_score(y_test, y_pred)
     print(f"Accuracy: {accuracy:.2f}")
 
@@ -25,10 +24,11 @@ def metrics(y_test, y_pred, y_pred_proba):
 
     f1 = f1_score(y_test, y_pred, zero_division=1)
     print(f"F1: {f1:.2f}")
-
+    
     fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
     roc_auc = auc(fpr, tpr)
-
+    print(f"AUC: {roc_auc:.2f}")
+    
     plt.figure()
     plt.plot(fpr, tpr, label=f'ROC curve (AUC = {roc_auc:.2f})')
     plt.plot([0, 1], [0, 1], color='black', linestyle='--', linewidth=2, label='Random Chance')
@@ -39,3 +39,5 @@ def metrics(y_test, y_pred, y_pred_proba):
     plt.title('ROC Curve for Hyperpartisan Classification')
     plt.legend()
     plt.show()
+
+    
